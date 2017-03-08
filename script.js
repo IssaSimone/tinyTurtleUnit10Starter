c132bb2a195909d638cce114e587ad91d5b1bc54
Issa Simone <iheard.student@hsfi.us>
2017-03-08 22:12:25 +0000

    tiny turtle

--- script.js
+++ script.js
@@ -22,37 +22,50 @@ right(120)
 forward(size)
 right(120)
 }
-
 triangle(32);
 triangle(94);
 
 function rectangle(size){
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 }
-rectangle();
+rectangle(60);
 
-function hexagon(){
+function hexagon(size){
     right(60);
-    forward(60);
+    forward(size);
     right(60);
-    forward(60);
+    forward(size);
     right(60);
-    forward(60);
+    forward(size);
     right(60);
-    forward(60);
+    forward(size);
     right(60);
-    forward(60);
+    forward(size);
     right(60);
 }
-hexagon();
+hexagon(53);
 
-function house(){
-    right(30);
-    forward(70);
+function house(size){
+    right(120);
+    forward(size);
+    right(120);
+    forward(size);
+    right(120);
+    forward(size);
+}
+ rectangle(40);
+ 
+function draw(shape,size){
+    hexagon(size);
+    rectangle(size);
+    triangle(size);
+    square(size);
+
+}
